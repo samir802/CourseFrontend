50605adaa27ea92b9a2cc89c0474c3874cbe7e5f
samir shrestha <shresthasammir@gmail.com>
2024-04-26 08:32:42 +0545

    Add project files.

---
+++ CourseFrontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CourseFrontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
