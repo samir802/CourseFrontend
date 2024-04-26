da2278eb61133fde738ce8be11a7730a67045656
samir shrestha <shresthasammir@gmail.com>
2024-04-26 09:47:10 +0545

    asdsa

--- CourseFrontend/Controllers/HomeController.cs
+++ CourseFrontend/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace CourseFrontend.Controllers
         {
             return View();
         }
+        public IActionResult profilePage()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
